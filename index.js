60f96aadbf6b3313436da6da239d222cf3546d42
mitaine7 <63794404+mitaine7@users.noreply.github.com>
2023-05-07 11:10:32 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -127,23 +127,28 @@ document.addEventListener("DOMContentLoaded", function () {
 const scroll = new LocomotiveScroll({
 el: document.querySelector("[data-scroll-container]"),
 smooth: true,
+smoothMobile: true,
+multiplier: 1.4, // Effect Multiplier
+touchMultiplier: 2,
+});
+
+/*const scroll = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true,
   lerp: 0.03, // Linear Interpolation, 0 > 1 // Try 0.01
   multiplier: 1.4, // Effect Multiplier
   reloadOnContextChange: true,
   touchMultiplier: 2,
-  smoothMobile: 1,
+  smoothMobile: 0,
   smartphone: {
-      smooth: !1,
+      smooth: !0,
       breakpoint: 767
   },
   tablet: {
       smooth: !1,
       breakpoint: 1024
   },
-});
-
-
-  
+});*/
 
 
 
