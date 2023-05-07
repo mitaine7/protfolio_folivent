2ab6cf649fabd630e5cb4fabb563cbe480038fd4
mitaine7 <63794404+mitaine7@users.noreply.github.com>
2023-05-07 18:06:54 -0200

    Add files via upload

--- index.js
+++ index.js
@@ -130,7 +130,7 @@ smooth: true,
 smoothMobile: true,
 multiplier: 1.4, // Effect Multiplier
 touchMultiplier: 2.22,
-lerp: .03, // Linear Interpolation, 0 > 1 // Try 0.01
+//lerp: .03, // Linear Interpolation, 0 > 1 // Try 0.01
 });
 
 /*const scroll = new LocomotiveScroll({
